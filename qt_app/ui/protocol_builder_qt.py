4bf031c8175dfd0222464fa95eca9590f41d4784
azln3n <rusyaliev9@gmail.com>
2026-01-30 01:01:38 +0300

    without ":"

--- qt_app/ui/protocol_builder_qt.py
+++ qt_app/ui/protocol_builder_qt.py
@@ -537,7 +537,7 @@ class ProtocolBuilderQt(QtCore.QObject):
         )
 
     def _create_field_widget(self, conn, meta: FieldMeta) -> FieldBinding:
-        label = QtWidgets.QLabel(meta.name + ":")
+        label = QtWidgets.QLabel(meta.name)
         # Чуть меньше расстояние между подписью и полем
         label.setStyleSheet("font-weight: bold; padding: 4px 4px;")
 
